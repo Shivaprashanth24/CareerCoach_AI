a159bb81169b03749f7aae38af74511036adb702
Shivaprashanth24 <prashanthshiva24@gmail.com>
2026-09-01 16:50:29 +0530

    Enhance MySQL connection parameters
    Updated the get_connection function to include port and ssl_disabled parameters.

--- modules/database.py
+++ modules/database.py
@@ -12,11 +12,13 @@ import config
 def get_connection():
     """Create and return a new MySQL connection."""
     return mysql.connector.connect(
-        host=config.MYSQL_HOST,
-        user=config.MYSQL_USER,
-        password=config.MYSQL_PASSWORD,
-        database=config.MYSQL_DB,
-    )
+    host=config.MYSQL_HOST,
+    port=config.MYSQL_PORT,
+    user=config.MYSQL_USER,
+    password=config.MYSQL_PASSWORD,
+    database=config.MYSQL_DB,
+    ssl_disabled=False,
+)
 
 
 # ---------------------------------------------------------------
